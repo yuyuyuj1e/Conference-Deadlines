b40e06394bee34eb37d6cc3fca65a1575ddbb3ab
yuyuyuj1e <807152541@qq.com>
2023-02-11 11:10:18 +0800

    添加微信公众号二维码

--- static/js/function.js
+++ static/js/function.js
@@ -1,14 +1,3 @@
-/*
- * @author: yuyuyuj1e 807152541@qq.com
- * @github: https://github.com/yuyuyuj1e
- * @csdn: https://blog.csdn.net/yuyuyuj1e
- * @date: 2023-02-07 16:21:12
- * @last_edit_time: 2023-02-08 14:06:52
- * @file_path: /Conference-Deadlines/static/js/function.js
- * @description: 头部注释配置模板
- */
-document.write("<script src='./jquery-3.6.3.min.js'></script>");
-
 $(".btn-check").change(function() {
     let check1 = $("#btncheck1").is(":checked")
     let check2 = $("#btncheck2").is(":checked")
@@ -21,4 +10,16 @@ $(".btn-check").change(function() {
        + "&check3=" + check3
        + "&check4=" + check4
        + "&search=" + search
+});
+
+$(document).ready(function() {
+    //图片鼠标悬停事件
+    $(function() {
+        $('#wechat').mouseover(function() {
+            $('#wechat-img').show();
+        })
+        $('#wechat').mouseleave(function() {
+            $('#wechat-img').hide();
+        })
+    })
 });
